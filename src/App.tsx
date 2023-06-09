7478bce5b3fad581e379ecad8c7cc50d1760ec21
Ashraf Mollah <mollah.dev@gmail.com>
2023-06-09 13:52:50 +0600

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import CrossIcon from "@icons/cross"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center">App file</h1>
+      <CrossIcon />
+    </>
+  )
+}
+
+export default App
